4235df44a542c6f403ee92a6355b749039265bb8
reneener <jhjin2u@gmail.com>
2022-11-21 20:19:31 +0900

    20221121 algo

--- src/BinarySearch/Boj_1654.java
+++ src/BinarySearch/Boj_1654.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package BinarySearch;
 
 import java.util.Arrays;
@@ -8,33 +13,35 @@ public class Boj_1654 {
 	static int target;
 	static int N;
 
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public Boj_1654() {
+	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		target = sc.nextInt();
-
 		lines = new int[N];
-		for(int i=0; i<N; i++){
+
+		for(int i = 0; i < N; ++i) {
 			lines[i] = sc.nextInt();
 		}
 
 		Arrays.sort(lines);
-
-		System.out.println(binarySearch(0, lines[N-1]));
+		System.out.println(binarySearch(0, lines[N - 1]));
 	}
+
 	private static int binarySearch(int start, int end) {
 		int half = (start + end) / 2;
-		if(start<=end){
+		if (start <= end) {
 			int sum = 0;
-			for(int i=0; i<N; i++) {
+
+			for(int i = 0; i < N; ++i) {
 				if (lines[i] > half) {
 					sum += lines[i] - half;
 				}
 			}
-			//if()
 		}
+
 		return 0;
 	}
-
 }
--- src/BinarySearch/Boj_2805.java
+++ src/BinarySearch/Boj_2805.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package BinarySearch;
 
 import java.util.Arrays;
@@ -7,41 +12,46 @@ public class Boj_2805 {
 	static int[] trees;
 	static int target;
 	static int N;
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		target = sc.nextInt();
-
 		trees = new int[N];
-		for(int i=0; i<N; i++){
+
+		for(int i = 0; i < N; ++i) {
 			trees[i] = sc.nextInt();
 		}
 
 		Arrays.sort(trees);
 		StringBuffer sb = new StringBuffer();
-		sb.append(binarySearch(0, trees[N-1]));
+		sb.append(binarySearch(0L, (long)trees[N - 1]));
 		System.out.println(sb);
-
 	}
+
 	private static long binarySearch(long left, long right) {
-		long mid = 0;
-		long result = 0;
-		while(left <= right){
-			System.out.println(left +" "+ right+" "+mid);
-			mid = (left + right) / 2;
-			long sum = 0;
-			for(int i=0; i<N; i++){
-				if(trees[i] > mid){
-					sum += trees[i] - mid;
+		long mid = 0L;
+		long result = 0L;
+
+		while(left <= right) {
+			System.out.println(left + " " + right + " " + mid);
+			mid = (left + right) / 2L;
+			long sum = 0L;
+
+			for(int i = 0; i < N; ++i) {
+				if ((long)trees[i] > mid) {
+					sum += (long)trees[i] - mid;
 				}
 			}
-			if(sum >= target){
-				left = mid + 1;
+
+			if (sum >= (long)target) {
+				left = mid + 1L;
 				result = mid;
-			}else
-				right = mid - 1;
+			} else {
+				right = mid - 1L;
+			}
 		}
+
 		return result;
 	}
 }
