3ddc0eaafcad7408cd9dae5e149ace58395c00a2
reneener <jhjin2u@gmail.com>
2022-11-22 13:39:21 +0900

    input 추가

--- src/TwoPointer/Boj_2003.java
+++ src/TwoPointer/Boj_2003.java
@@ -1,2 +1,17 @@
-package TwoPointer;public class Boj_2003 {
+package TwoPointer;
+
+import java.util.Scanner;
+
+public class Boj_2003 {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int target = in.nextInt();
+		int[] nums = new int[N];
+		for(int i=0; i<N; i++){
+			nums[i] = in.nextInt();
+		}
+
+		
+	}
 }
