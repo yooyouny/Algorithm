9b77602e28fa0c8249ae4318ff4a2cf522925649
reneener <jhjin2u@gmail.com>
2022-11-29 12:56:36 +0900

    Revert "Revert "221129""
    This reverts commit 726f68c70a79fdaf479ff70ca9c6d054d2fa5b31.

--- src/binarysearch/Boj_10816.java
+++ src/binarysearch/Boj_10816.java
@@ -1,78 +1,64 @@
-package binarysearch;
+
+package BinarySearch;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * 복습
- */
 public class Boj_10816 {
 	static int[] A;
 	static int[] B;
+
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
 		int N = sc.nextInt();
 		int[] arr = new int[N];
 
-		for(int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
+		int M;
+		for(M = 0; M < N; ++M) {
+			arr[M] = sc.nextInt();
 		}
 
-		Arrays.sort(arr);	// 이분 탐색을 하기 위해서는 반드시 정렬되어있어야 한다.
-
-		int M = sc.nextInt();
-
+		Arrays.sort(arr);
+		M = sc.nextInt();
 		StringBuilder sb = new StringBuilder();
 
-		for(int i = 0; i < M; i++) {
+		for(int i = 0; i < M; ++i) {
 			int key = sc.nextInt();
-
-			//중복 원소에 대한 길이 = 상한 - 하한
 			sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
 		}
+
 		System.out.println(sb);
 	}
 
-
-	//찾고자 하는 값 이상의 값이 처음으로 나타나는 위치
 	private static int lowerBound(int[] arr, int key) {
-		int left=0;
-		int right=arr.length;
+		int left = 0;
+		int right = arr.length;
 
-		while(left<right) {
-			int mid=(left+right)/2;
-
-			if(key<=arr[mid]) { //상한선 내리기
-				right=mid;
-			}
-			else {
-				left=mid+1;
+		while(left < right) {
+			int mid = (left + right) / 2;
+			if (key <= arr[mid]) {
+				right = mid;
+			} else {
+				left = mid + 1;
 			}
 		}
 
 		return left;
 	}
 
-	//찾고자 하는 값 초과의 값이 처음으로 나타나는 위치
 	private static int upperBound(int[] arr, int key) {
-		int left=0;
-		int right=arr.length;
+		int left = 0;
+		int right = arr.length;
 
-		while(left<right) {
-			int mid=(left+right)/2;
-
-			if(key<arr[mid]) {
-				right=mid;
-			}
-			else {//하한선 올리기
-				left=mid+1;
+		while(left < right) {
+			int mid = (left + right) / 2;
+			if (key < arr[mid]) {
+				right = mid;
+			} else {
+				left = mid + 1;
 			}
 		}
 
 		return left;
 	}
 }
-
-
--- src/binarysearch/Boj_1654.java
+++ src/binarysearch/Boj_1654.java
@@ -1,4 +1,9 @@
-package binarysearch;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package BinarySearch;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -8,37 +13,35 @@ public class Boj_1654 {
 	static int target;
 	static int N;
 
+	public Boj_1654() {
+	}
+
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		N = in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		target = sc.nextInt();
 		lines = new int[N];
-		target = in.nextInt();
-		for(int i=0; i<N; i++){
-			lines[i] = in.nextInt();
+
+		for(int i = 0; i < N; ++i) {
+			lines[i] = sc.nextInt();
 		}
 
 		Arrays.sort(lines);
-
-		System.out.println(binarySearch(1, 1000000));
+		System.out.println(binarySearch(0, lines[N - 1]));
 	}
-	public static long binarySearch(long left, long right) {
-		long result = 0;
-		while(left<=right){
-			long sum = 0;
-			long mid = (left + right) / 2;
-			for(int i=0; i<N; i++){
-				if(lines[i]>mid){
-					sum+= lines[i] / mid;
+
+	private static int binarySearch(int start, int end) {
+		int half = (start + end) / 2;
+		if (start <= end) {
+			int sum = 0;
+
+			for(int i = 0; i < N; ++i) {
+				if (lines[i] > half) {
+					sum += lines[i] - half;
 				}
 			}
-			if(sum>=target){
-				left = mid + 1;
-				result = mid;
-			}else{
-				right = mid;
-			}
 		}
-		return result;
 
+		return 0;
 	}
 }
