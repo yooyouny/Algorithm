4d0fe6381e8207052516ace4cc7daaeba066b5fb
harper <35358294+yooyouny@users.noreply.github.com>
2023-02-23 15:51:58 +0900

    [level 2] Title: 타겟 넘버, Time: 4.57 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 3.83 ms
+메모리: 10.2 MB, 시간: 4.57 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,29 +1,22 @@
-cnt = 0
-
-def dfs(numbers, visited,index, target, sum):
-    global cnt
-    if index == len(numbers):
-        if target == sum:
-            cnt += 1
-        return 
-    
-    visited[index] = 1
-
-    sum += numbers[index]
-    dfs (numbers, visited, index + 1, target, sum)
-    sum -= numbers[index]
-        
-    sum -= numbers[index]
-    dfs (numbers, visited, index + 1, target, sum)
-    sum += numbers[index]
-    
-    visited[index] = 0
-    
-    return
-        
-    
+sum = 0
+answer = 0
 
 def solution(numbers, target):
-    visited = [0] * len(numbers)
-    dfs(numbers, visited, 0, target,  0)
-    return cnt
+    
+    def dfs(idx):
+        global sum, answer
+        if idx == len(numbers):
+            if sum == target:
+                answer += 1
+            return
+       
+        sum += numbers[idx]
+        dfs(idx+1)
+        sum -= numbers[idx]
+    
+        sum -= numbers[idx]
+        dfs(idx+1)
+        sum += numbers[idx]
+    
+    dfs(0)
+    return answer
