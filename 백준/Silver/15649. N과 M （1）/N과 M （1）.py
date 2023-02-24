3e3b2e3190407e2626adb9e226106ce7bd8bb170
harper <35358294+yooyouny@users.noreply.github.com>
2023-02-24 15:24:47 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,14 @@
-N, M = map(int, input().split())
-num = []
+n, m = map(int, input().split())
+list = []
 def dfs():
-  if len(num) == M:
-    print(' '.join(map(str, num)))
+  if len(list) == m:
+    print(" ".join(map(str, list))) #공백을 사이에 두고 출력
     return
 
-  for i in range(1, N+1):
-    if i in num:
-      continue
-    num.append(i)
-    dfs()
-    num.pop()
+  for i in range(1, n+1): #1부터 n까지
+    if i not in list: # 중복없이 
+      list.append(i)
+      dfs()
+      list.pop()
 
 dfs()
