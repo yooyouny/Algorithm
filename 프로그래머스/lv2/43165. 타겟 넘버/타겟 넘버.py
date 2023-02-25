1a8341231de6539140b5b26f1796f93e7ae1620d
harper <35358294+yooyouny@users.noreply.github.com>
2023-02-25 12:59:28 +0900

    [level 2] Title: 타겟 넘버, Time: 4.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,21 +2,20 @@ sum = 0
 answer = 0
 
 def solution(numbers, target):
-    
-    def dfs(idx):
-        global sum, answer
-        if idx == len(numbers):
-            if sum == target:
-                answer += 1
-            return
-       
-        sum += numbers[idx]
-        dfs(idx+1)
-        sum -= numbers[idx]
-    
-        sum -= numbers[idx]
-        dfs(idx+1)
-        sum += numbers[idx]
-    
-    dfs(0)
-    return answer
+  def dfs(idx):
+    global sum, answer
+    if idx == len(numbers):
+      if sum == target:
+        answer += 1
+      return
+
+    sum += numbers[idx]
+    dfs(idx+1)
+    sum -= numbers[idx]
+
+    sum -= numbers[idx]
+    dfs(idx+1)
+    sum += numbers[idx]
+
+  dfs(0)
+  return answer
