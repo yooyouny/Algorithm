e51df68aba5c5b66d82b7c65f7edba0fdd8f5683
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-02 23:43:54 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,17 +1,16 @@
-
 def solution(n, computers):
     answer = 0
     visited = [False] * n
-    
-    def dfs(x):
-        visited[x] = True
+ 
+    def dfs(next):
+        visited[next] = True
         for i in range(n):
-            if not visited[i] and computers[x][i] == 1:
+            if computers[next][i] == 1 and not visited[i]:
                 dfs(i)
-    
-    for i in range(len(computers)):
-        for j in range(len(computers)):
-            if not visited[i] and computers[i][j] == 1:
-                dfs(i)
+        
+    for i in range(n):
+        for j in range(n):
+            if not visited[j] and computers[i][j] == 1:
+                dfs(j)
                 answer += 1
     return answer
