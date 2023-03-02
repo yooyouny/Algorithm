f82ca1f9587509a4976167e74c112c7a5188630f
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-03 04:13:43 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,7 +1,15 @@
-SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
-FROM FOOD_PRODUCT
-WHERE (CATEGORY, PRICE) in (SELECT CATEGORY, max(PRICE)
-                            FROM FOOD_PRODUCT 
-                            WHERE CATEGORY in ('과자', '국', '김치', '식용유')
-                            GROUP BY CATEGORY)
-ORDER BY MAX_PRICE desc
+/*
+1. 식품의 분류 별로 -> Group by 생각함
+2. groupby에 특정 기준을 쓰면 반드시 select에도 써줘야 한다고 생각했음 
+-> 집계함수만 select할 경우 생략 가능함 
+3. where절 서브쿼리 시 =을 쓸 경우 서브쿼리의 결과가 하나여야함 
+-> in으로 대체
+*/
+select CATEGORY, PRICE, PRODUCT_NAME
+from FOOD_PRODUCT
+where PRICE in (select max(PRICE)
+                from FOOD_PRODUCT
+                group by category)
+AND CATEGORY in ('과자', '국', '김치', '식용유')
+order by price desc 
+        
