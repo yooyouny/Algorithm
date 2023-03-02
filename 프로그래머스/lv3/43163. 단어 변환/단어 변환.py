11248204b49966604d2a99158314fc658778a7e4
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-03 00:06:13 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -3,23 +3,20 @@ from collections import deque
 def solution(begin, target, words):
     if target not in words:
         return 0
-
+    
     queue = deque()
     queue.append((begin, 0))
     
     while queue:
-        request, cnt = queue.popleft()
-        if request == target:
+        start_str, cnt = queue.popleft()
+        if start_str == target:
             return cnt
-        
         for word in words:
-            #사전에 있는 단어가 변환 가능한지 
-            chk = 0
-            for j in range(len(word)):
-                if request[j] != word[j]: 
-                    chk += 1
-                    
-            if chk == 1: # 단어변환이 가능함 
+            diff_cnt = 0
+            for idx in range(len(word)):
+                if word[idx] != start_str[idx]:
+                    diff_cnt += 1
+            if diff_cnt == 1:
                 queue.append((word, cnt + 1))
-            
-    return cnt
+
+    
