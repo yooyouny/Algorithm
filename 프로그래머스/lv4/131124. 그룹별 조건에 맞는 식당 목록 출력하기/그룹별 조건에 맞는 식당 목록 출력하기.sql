bff1c0572705b46506a10801238cf283f9a47b1a
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-03 12:48:53 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,21 +1,13 @@
--- 코드를 입력하세요
-SELECT MEMBER_NAME, B.REVIEW_TEXT, date_format(B.REVIEW_DATE, '%Y-%m-%d')
-FROM REST_REVIEW B
-JOIN(
-         SELECT   I.MEMBER_ID
-                , I.REVIEW_DATE
-                , I.REVIEW_TEXT
-                , M.MEMBER_NAME
-                , RANK() OVER (ORDER BY CNT DESC) AS RK
-         FROM   ( SELECT *
-                 , COUNT(MEMBER_ID) as CNT
-                  FROM REST_REVIEW
-                  GROUP BY MEMBER_ID
-                ) I
-        JOIN MEMBER_PROFILE M ON I.MEMBER_ID = M.MEMBER_ID
-     ) A #가장 리뷰 수가 많은 멤버 출력 
-ON A.MEMBER_ID = B.MEMBER_ID
-WHERE 1=1
-AND A.RK = 1
-ORDER BY B.REVIEW_DATE, B.REVIEW_TEXT
-;
+select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
+from MEMBER_PROFILE m, REST_REVIEW r
+where m.member_id = r.member_id
+and m.member_id in (select member_id
+                  from REST_REVIEW
+                  group by member_id
+                 having count(*) in (select max(cnt) 
+                                    from (select count(*) as cnt
+                                     from rest_review
+                                     group by member_id)c
+                                     )
+                    )
+order by REVIEW_DATE, REVIEW_TEXT
