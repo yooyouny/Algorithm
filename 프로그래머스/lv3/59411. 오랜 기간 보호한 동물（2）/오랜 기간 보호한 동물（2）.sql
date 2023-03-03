2216c850ab6f6fa046d1552dc12bef2833672dd7
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-03 15:15:49 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,33 @@
-SELECT I.ANIMAL_ID, I.NAME
-FROM ANIMAL_INS I
-JOIN ANIMAL_OUTS O on I.ANIMAL_ID = O.ANIMAL_ID
-ORDER BY (O.DATETIME - I.DATETIME) desc
-LIMIT 2
+/*with max_diff as (
+(select i.ANIMAL_ID, (datediff(o.datetime, i.datetime) + 1) as diff
+                                from ANIMAL_INS i, ANIMAL_OUTS o
+                                where i.animal_id = o.animal_id
+                                group by i.ANIMAL_ID
+                            )
+)
+
+select i.ANIMAL_ID, i.NAME
+from ANIMAL_INS i, ANIMAL_OUTS o
+where i.animal_id = o.animal_id
+and (i.animal_id, datediff(o.datetime, i.datetime) + 1) in (
+                        select i.ANIMAL_ID, max(diff)
+                        from max_diff
+                        )
+group by i.ANIMAL_ID, i.NAME*/
+
+/*가장 오래된 동물 두마리  
+Girly Girl	350
+Emily	293
+select i.name, datediff(o.datetime, i.datetime)+1 as diff
+from ANIMAL_INS i, ANIMAL_OUTS o
+where i.animal_id = o.animal_id 
+order by diff desc
+limit 2
+*/
+select i.ANIMAL_ID, i.NAME
+from ANIMAL_INS i, ANIMAL_OUTS o
+where i.animal_id = o.animal_id
+order by datediff(o.datetime, i.datetime)+1 desc
+limit 2
+
+
