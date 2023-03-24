fbc956fdbde915b6f024cc0c9ba924ce8e4254bb
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-25 06:30:17 +0900

    [level 2] Title: 모음 사전, Time: 1.64 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,25 +1,23 @@
-cnt = 0
 result = 0
+cnt = 0
 def solution(word):
-    def dfs(list):
-        global result, cnt
-
-        if "".join(map(str, list)) == word:
+    list = []
+    def dfs():
+        global cnt, result
+        if "".join(list) == word:
             result = cnt
             return
-        
+
         if len(list) == 5:
             return
-        
+
         alpha = ['A', 'E', 'I', 'O', 'U']
-        
+
         for i in range(5):
             cnt += 1
             list.append(alpha[i])
-            dfs(list)
+            dfs()
             list.pop()
-            
-            
-    dfs([])
+
+    dfs()
     return result
- 
