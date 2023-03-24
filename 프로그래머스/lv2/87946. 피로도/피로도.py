b85faddaeacf5af01c1de1cea4fd085b24d7205b
harper <35358294+yooyouny@users.noreply.github.com>
2023-03-25 07:27:20 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,16 +1,14 @@
 from itertools import permutations
-
 def solution(k, dungeons):
-    idx = list(range(len(dungeons)))
-    candidates = list(permutations(idx, len(dungeons)))
-    max_dungeon = -1
-    for candidate in candidates: # 012 021 102
-        dungeon = 0
+    max_value = -1e9
+    candidates = list(permutations(dungeons, len(dungeons)))
+    for candidate in candidates:
+        answer = 0
         now = k
-        for index in candidate: # 0 2 1
-            minimum, consume = dungeons[index]
+        for minimum, consume in candidate:
             if now >= minimum:
                 now -= consume
-                dungeon += 1
-        max_dungeon = max(max_dungeon, dungeon)
-    return max_dungeon
+                answer += 1
+                continue
+        max_value = max(max_value, answer)
+    return max_value
