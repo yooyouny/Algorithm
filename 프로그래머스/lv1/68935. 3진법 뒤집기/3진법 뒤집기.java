d74544f61108596f774edeba504a09127787e376
harper <35358294+yooyouny@users.noreply.github.com>
2023-04-29 02:45:53 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int n) {
-        System.out.println(Integer.toString(n, 3));
         StringBuilder sb = new StringBuilder(Integer.toString(n, 3));
         return Integer.parseInt(sb.reverse().toString(), 3);
     }
