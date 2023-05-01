e570e78c44035efc97ec2ec1c3f387c27ea7cd55
harper <35358294+yooyouny@users.noreply.github.com>
2023-05-01 21:23:06 +0900

    [level 2] Title: 문자열 압축, Time: 0.07 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -7,7 +7,7 @@ class Solution {
             String pattern = s.substring(0, i);
             int cnt = 1;
             
-            for(int j=i; j<s.length(); j+=i){
+            for(int j=i; j<=s.length(); j+=i){
                 if(pattern.equals(s.substring(j, Math.min(j+i, s.length())))){
                     cnt++;
                 }else{
@@ -17,7 +17,6 @@ class Solution {
                     cnt = 1;
                 }
             }
-            if(cnt > 1) sb.append(cnt);
             sb.append(pattern);
             answer = Math.min(answer, sb.length());
         }
