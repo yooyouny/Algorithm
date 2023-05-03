4e0930ed0417cf704e071b5f48a9fe61e6098ca2
harper <35358294+yooyouny@users.noreply.github.com>
2023-05-03 13:40:38 +0900

    [level 2] Title: 소수 찾기, Time: 2.43 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    Set<Integer> set;
+    public void comb(char[] numbers, boolean[] visited, StringBuilder sb){
+        if(sb.length() > 0){
+            set.add(Integer.parseInt(sb.toString()));
+        }
+            
+        for(int i=0; i<numbers.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                sb.append(numbers[i]);
+                comb(numbers, visited, sb);
+                sb.deleteCharAt(sb.length() - 1);
+                visited[i] = false;
+            }   
+        }
+    }
+    public boolean isPrime(int n){
+        if(n < 2) return false;
+        
+        for(int i=2; i<=n/2; i++){
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public int solution(String numbers) {
+
+        int cnt = 0;
+        set= new HashSet<>();
+        comb(numbers.toCharArray(), new boolean[numbers.length()], new StringBuilder());
+        for(int n : set){
+            if(isPrime(n))
+                cnt++;
+        }
+        return cnt;
+    }
+}
