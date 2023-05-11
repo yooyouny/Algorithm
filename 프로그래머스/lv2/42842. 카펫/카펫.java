a90008a3213b6e508e074bd058b8bac8173b8ecb
harper <35358294+yooyouny@users.noreply.github.com>
2023-05-11 19:05:23 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -3,7 +3,7 @@ class Solution {
         int[] answer = {0, 0};
         int total = brown + yellow;
 
-        for(int i=2; i<=Math.sqrt(total); i++){
+        for(int i=3; i<=Math.sqrt(total); i++){
             if(total % i == 0){
                 int j = total / i;
                 if((i-2) * (j-2) == yellow){
