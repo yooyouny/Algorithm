0a89c34cebe016570d293a0429175b7844b4bce6
harper <35358294+yooyouny@users.noreply.github.com>
2023-06-02 18:01:08 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.31 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Point {
+class Point{
     long x;
     long y;
     public Point(long x, long y){
@@ -7,76 +7,78 @@ class Point {
         this.y = y;
     }
 }
-class Solution {
-    long[] result;
-    public Boolean findIntersection(int[] first, int[] second){
-        
+class Solution{
+    List<Point> points;
+
+    private void getPoint(int[] first, int[] second){
         long a = first[0];
-        long b = first[1];
-        long e = first[2];
-        long c = second[0];
-        long d = second[1];
-        long f = second[2];
+		long b = first[1];
+		long e = first[2];
+		long c = second[0];
+		long d = second[1];
+		long f = second[2];
         
         long adbc = (a*d) - (b*c);
-        long bfed = (b*f) - (e*d);
-        long ecaf = (e*c) - (a*f);
+		long bfed = (b*f) - (e*d);
+		long ecaf = (e*c) - (a*f);
+
+        if(adbc == 0 || bfed % adbc != 0 || ecaf % adbc != 0) return;
         
-        if(adbc == 0){
-            return false;
-        }else if(bfed % adbc != 0 || ecaf % adbc != 0){
-            return false;
-        }else{
-            result[0] = bfed / adbc;
-            result[1] = ecaf / adbc;
-            return true;
-        }
+        long x = bfed / adbc;
+        long y = ecaf / adbc;
+        points.add(new Point(x, y));
+        
     }
-    
-    public String[] solution(int[][] line) {
-        Set<Point> points = new HashSet<>();
-        result = new long[2];
+    private String[][] printStar(){
+		long minX = Long.MAX_VALUE;
+		long minY = Long.MAX_VALUE;
+		long maxX = Long.MIN_VALUE;
+		long maxY = Long.MIN_VALUE;
+
+		for(Point p : points){
+			minX = Math.min(minX, p.x);
+			minY = Math.min(minY, p.y);
+			maxX = Math.max(maxX, p.x);
+			maxY = Math.max(maxY, p.y);
+		}
         
-        long minX = Long.MAX_VALUE;
-        long minY = Long.MAX_VALUE;
-        long maxX = Long.MIN_VALUE; 
-        long maxY = Long.MIN_VALUE;
+        int width = (int)(maxX - minX + 1);
+        int height = (int)(maxY - minY + 1);
         
-        for (int i = 0; i < line.length - 1; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                if(findIntersection(line[i], line[j])) //정수로 표현된 교점 좌표 구하기
-                    points.add(new Point(result[0], result[1]));
+        String[][] result = new String[height][width];
+        
+        for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+                result[i][j] = ".";
             }
         }
         
         for(Point p : points){
-            minX = Math.min(minX, p.x);
-            minY = Math.min(minY, p.y);
-            maxX = Math.max(maxX, p.x);
-            maxY = Math.max(maxY, p.y);
+            result[(int)(maxY - p.y)][(int)(p.x - minX)] = "*";
         }
         
-        boolean[][] stars = new boolean[(int)(maxY - minY + 1)][(int)(maxX - minX + 1)];
+        return result;
+    }
+    public String[] solution(int[][] line) {
         
-        for(Point p : points){
-            int x = (int)(p.x - minX);
-            int y = (int)(maxY - p.y);
-            stars[y][x] = true;
+        points = new ArrayList<>();
+        for(int i=0; i<line.length-1; i++){
+            for(int j=i+1; j<line.length; j++){
+                getPoint(line[i], line[j]);
+            }
         }
-        
+        String[][] stars = printStar();
+    
         String[] answer = new String[stars.length];
         int idx = 0;
-        
-        for(boolean[] star : stars){
+        for(int i=0; i<stars.length; i++){
             StringBuilder sb = new StringBuilder();
-            for(boolean chk : star){
-                if(chk)
-                    sb.append("*");
-                else
-                    sb.append(".");
+            for(int j=0; j<stars[i].length; j++){
+                sb.append(stars[i][j]);
             }
             answer[idx++] = sb.toString();
         }
+        
         return answer;
     }
 }
