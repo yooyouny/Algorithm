42ca30c7507d696ed433819cbc9c01d2025c3749
harper <35358294+yooyouny@users.noreply.github.com>
2023-06-03 02:45:07 +0900

    [level 2] Title: 삼각 달팽이, Time: 32.41 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,41 +1,34 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        int size = (n+1) * n / 2; 
+        int size = n * (n+1) / 2;
         int[] answer = new int[size];
-        int idx = 0;
         int[][] snail = new int[n][n];
         
-        int[] dx = {0, 1, -1}; //아래, 오른쪽, 대각선 위 순으로 진행 
+        int num = 1;
+        int x = 0, y = 0, idx = 0;
+        int[] dx = {0, 1, -1};
         int[] dy = {1, 0, -1};
         
-        int num = 1;
-        int y = 0, x = 0, nx = 0, ny = 0, angle = 0;
-        
-        while(num<=size){
+        while(num <= size){
             snail[y][x] = num;
-            ny = y + dy[angle];
-            nx = x + dx[angle];
-            num++;
+            int nx = x + dx[idx];
+            int ny = y + dy[idx];
             
-            if(0 <= ny && ny < n && 0 <= nx && nx <= ny && snail[ny][nx] == 0){
+            if(nx<0 || ny <0 || nx>=n || ny>=n || snail[ny][nx] != 0){
+                idx = (idx + 1) % 3;
+                y = y + dy[idx];
+                x = x + dx[idx];
+            }else{
                 y = ny;
                 x = nx;
-            }else{
-                angle = (angle + 1) % 3; 
-                y = y + dy[angle];
-                x = x + dx[angle];
-            }
+            }   
+            num++;
         }
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(snail[i][j] == 0)
-                    break;
-                else
-                    answer[idx++] = snail[i][j];
-            }
-        }
+        answer = Arrays.stream(snail)
+                    .flatMapToInt(Arrays::stream)
+                    .filter(value -> value != 0)
+                    .toArray();
         return answer;
     }
 }
