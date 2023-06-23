97bd1b037df283377eac5911a6e5040e295fa7c2
harper <35358294+yooyouny@users.noreply.github.com>
2023-06-23 22:20:50 +0900

    [Gold V] Title: 빗물, Time: 256 ms, Memory: 18272 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int w = sc.nextInt();
+		int[] ground = new int[w];
+		for(int i=0; i<w; i++) {
+			ground[i] = sc.nextInt();
+		}
+		int amount = 0;
+		for(int i=1; i<w-1; i++){
+			int leftHeight = 0;
+			int rightHeight = 0;
+
+			for(int j=i-1; j>=0; j--){
+				leftHeight = Math.max(leftHeight, ground[j]);
+			}
+			for(int j=i+1; j<w; j++){
+				rightHeight = Math.max(rightHeight, ground[j]);
+			}
+			if(leftHeight > ground[i] && rightHeight > ground[i])
+				amount += Math.min(leftHeight, rightHeight) - ground[i];
+		}
+		
+		System.out.println(amount);
+	}
+}
