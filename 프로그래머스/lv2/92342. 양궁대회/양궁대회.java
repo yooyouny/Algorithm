9727fed8a2606640b9d90dcd46055e05f2c9a1a6
harper <35358294+yooyouny@users.noreply.github.com>
2023-08-10 18:47:53 +0900

    [level 2] Title: 양궁대회, Time: 91.49 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -1,33 +1,18 @@
 import java.util.Arrays;
 
 public class Solution {
-    private int getScoreDiff(int[] apeach, int[] ryan) {
-        int diff = 0;
-        for (int i = 0; i < apeach.length; i++) {
-            if (apeach[i] == 0 && ryan[i] == 0) continue;
-
-            if (apeach[i] >= ryan[i]) {
-                diff -= 10 - i;
-            } else {
-                diff += 10 - i;
-            }
+    public int[] solution(int n, int[] info) {
+        int[] ryan = dfs(0, new int[11], n, info);
+        if (ryan == null) {
+            return new int[] { -1 };
         }
-        return diff;// 라이언과 어피치의 점수차 리턴
+        return ryan;
     }
-
-    private boolean isPrior(int[] base, int[] comp) {
-        for (int i = 10; i >= 0; i--) {
-            if (comp[i] == base[i]) continue;
-            return comp[i] > base[i];
-        }
-        return false;
-    }
-
     private int[] dfs(int index, int[] score, int n, int[] apeach) {
         if (index == score.length) {
-            if (n > 0) return null;
+            //if (n > 0) return null;
             if (getScoreDiff(apeach, score) <= 0) return null; // 어피치가 이긴경우 
-            return Arrays.copyOf(score, score.length);
+            return Arrays.stream(score).toArray();
         }
 
         int maxDiff = 0;
@@ -39,8 +24,7 @@ public class Solution {
             if (ryan == null) continue;
 
             int diff = getScoreDiff(apeach, ryan);
-            if (diff > maxDiff ||
-                    (diff == maxDiff && isPrior(result, ryan))) {
+            if (diff > maxDiff || (diff == maxDiff && isValid(result, ryan))) {
                 maxDiff = diff;
                 result = ryan;
             }
@@ -48,12 +32,23 @@ public class Solution {
 
         return result;
     }
-
-    public int[] solution(int n, int[] info) {
-        int[] ryan = dfs(0, new int[11], n, info);
-        if (ryan == null) {
-            return new int[] { -1 };
+    private int getScoreDiff(int[] apeach, int[] ryan) {
+        int diff = 0;
+        for (int i = 0; i < apeach.length; i++) {
+            if (apeach[i] == 0 && ryan[i] == 0) continue;
+            if (apeach[i] >= ryan[i]) {
+                diff -= 10 - i;
+            } else {
+                diff += 10 - i;
+            }
         }
-        return ryan;
+        return diff;
+    }
+    private boolean isValid(int[] result, int[] ryan) {
+        for (int i = 10; i >= 0; i--) {
+            if (ryan[i] == result[i]) continue;
+            return ryan[i] > result[i];
+        }
+        return false;
     }
 }
