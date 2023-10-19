b4939ff30f518731e54cd6e793057f7756048e12
harper <35358294+yooyouny@users.noreply.github.com>
2023-10-19 19:53:54 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.56 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,32 +1,28 @@
+import java.util.*;
 class Solution {
       public long solution(int[] weights) {
-        long answer = 0;
-
-        long[] cntWeight = new long[1001];
-        long[] cntMul = new long[4001];
-
-        for(int i = 0; i < weights.length; i ++) {
-            long temp = cntWeight[weights[i]];
-            int m2 = weights[i] * 2;
-            int m3 = weights[i] * 3;
-            int m4 = weights[i] * 4;
-            if(temp > 0) {
-                answer += temp;
-                answer += cntMul[m2] - temp;
-                answer += cntMul[m3] - temp;
-                answer += cntMul[m4] - temp;
-            } else {                       
-                answer += cntMul[m2];
-                answer += cntMul[m3];
-                answer += cntMul[m4];
-            }
-
-            cntWeight[weights[i]] ++;
-            cntMul[m2] ++;
-            cntMul[m3] ++;
-            cntMul[m4] ++;
+  	    long answer = 0;
+        Arrays.sort(weights);
+        Map<Double, Integer> map = new HashMap<>();
+        
+          for(int w : weights) {
+    		double weight = w * 1.0;
+    		double m2 = (w*2.0) / 3.0;
+    		double m3 = (w*1.0) / 2.0;
+    		double m4 = (w*3.0) / 4.0;
+            
+    		if(map.containsKey(weight)) 
+                answer += map.get(weight);
+    		if(map.containsKey(m2)) 
+                answer += map.get(m2);
+    		if(map.containsKey(m3)) 
+                answer += map.get(m3);
+    		if(map.containsKey(m4)) 
+                answer += map.get(m4);
+    		
+            map.put((w*1.0), map.getOrDefault((w*1.0), 0) + 1);
         }
-
+        
         return answer;
     }
 }
