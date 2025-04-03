2d3779cb6828fb1484d4eb5ec92e8331b24e4e90
harper <35358294+yooyouny@users.noreply.github.com>
2025-04-03 20:37:32 +0900

    [level 1] Title: 유연근무제, Time: 0.61 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        boolean[] present = new boolean[schedules.length];
+        int[] endTimeSchedules = getEndTime(schedules);
+        for(int i=0; i<timelogs.length; i++){
+            int[] days = timelogs[i];
+            for(int j=0; j<days.length; j++){
+                int dayOfWeek = (startday + j) % 7;
+                if(dayOfWeek == 6 || dayOfWeek == 0) continue;
+                if(endTimeSchedules[i]>=days[j]) 
+                    present[i] = true;
+                else{
+                    present[i] = false;
+                    break;
+                }
+            }
+        }
+        for(int i=0; i<present.length; i++){
+            if(present[i])
+                answer++;
+        }
+        return answer;
+    }
+    public int[] getEndTime(int[] schedules){
+        int n = schedules.length;
+        int[] endTimes = new int[n];
+        for(int i=0; i<n; i++){
+            int hour = schedules[i] / 100;
+            int minute = schedules[i] % 100 + 10;
+            if(minute > 59){
+                hour++;
+                minute %= 60;
+            }
+            //System.out.println(i+ " 번째 출근인정시각 : "+ hour + " " + minute);
+            endTimes[i] = (hour * 100) + minute;
+        }
+        return endTimes;
+    }
+}
